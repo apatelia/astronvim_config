ea99b09959a09173ef84bea6dbed4b26026ce944
Arpan Patelia <arpan.is@gmail.com>
2024-12-08 00:00:42 +0530

    Make astronvim config compatible with VS Code

--- lua/community.lua
+++ lua/community.lua
@@ -17,14 +17,13 @@ return {
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
 
-  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
   { import = "astrocommunity.editing-support.suda-vim" },
-  { import = "astrocommunity.editing-support.multiple-cursors-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.editing-support.cutlass-nvim" },
-
-  { import = "astrocommunity.workflow.precognition-nvim" },
 
   { import = "astrocommunity.search.grug-far-nvim" },
 
   { import = "astrocommunity.test.neotest" },
+
+  { import = "astrocommunity.recipes.vscode" },
 }
