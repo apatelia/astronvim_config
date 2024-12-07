11c5f5292241ee78493b8d774531e10a178a56bb
Arpan Patelia <arpan.is@gmail.com>
2024-12-07 18:51:52 +0530

    Override neo-tree to display expanders

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -9,6 +9,8 @@ return {
           -- second key is the lefthand side of the map
           -- mappings seen under group name "Buffer"
           ["<Leader>bn"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+
+          -- mappings seen under group name "Test"
           ["<Leader>Ts"] = { function() require('neotest').summary.toggle() end, desc = "Test Summary" },
           ["<Leader>Ta"] = { function() require('neotest').playwright.attachment() end, desc = "Launch test attachment" },
         },
